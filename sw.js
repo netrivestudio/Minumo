ad86e9bcd71e45b7cc7a607d45c721950f774caa
netrivestudio <netrivestudio@gmail.com>
2026-01-22 19:01:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
 });
 
-self.addEventListener('fetch', event => {
-  // nanti bisa ditambah caching offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
