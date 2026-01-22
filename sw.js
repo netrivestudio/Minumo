a312b2a2ce59c9237bf6fd71223b34affecca208
netrivestudio <netrivestudio@gmail.com>
2026-01-22 20:31:13 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "minumo-cache-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
